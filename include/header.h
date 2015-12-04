f8781920792f659bbe9d423300f47eaef032083a
Kevin <kevin.borras.soler@gmail.com>
2015-12-04 11:39:44 +0100

    Update header.h

--- include/header.h
+++ include/header.h
@@ -1,7 +1,7 @@
 /*****************************************************************************/
 /**   Ejemplo de un posible fichero de cabeceras ("header.h") donde situar  **/
 /** las definiciones de constantes, variables y estructuras para MenosC.16  **/
-/** Los alumos deberan adaptarlo al desarrollo de su propio compilador.     **/ 
+/** Los alumos deberan adaptarlo al desarrollo de su propio compilador.     **/
 /*****************************************************************************/
 #ifndef _HEADER_H
 #define _HEADER_H
@@ -9,12 +9,15 @@
 #define TRUE  1
 #define FALSE 0
 #define TALLA_TIPO_SIMPLE     1
+#define MAX_LENGTH 14
 /************************************* Variables externas definidas en el AL */
 extern FILE *yyin;
 extern int   yylineno;
 extern int   yydebug;
 extern char *yytext;
 extern int   yyleng;
+extern int   yylval;
+
 /********************* Variables externas definidas en el Programa Principal */
 extern int verbosidad;              /* Flag para saber si se desea una traza */
 extern int numErrores;              /* Contador del numero de errores        */
@@ -24,7 +27,7 @@ extern int verTDS;                  /* Flag para saber si mostrar la TDS     */
 extern int dvar;              /* Desplazamiento en el Segmento de Variables  */
 
 /********************************************************************************/
- /* Comprueba que el identificador no exceda la talla maxima (14) o lo trunca    */
+ /* Comprueba que el identificador no exceda la talla maxima (14) o lo trunca    */
 void creaNombre(){
 
 	char *t;
@@ -43,13 +46,18 @@ void creaCentera(){
 	yylval.cent = atoi(yytext);
 	}
 /********************************************************************************/
-/* Transforma una subcadena a una constante entera truncandola.                 */
+/* Transforma una subcadena a una constante entera truncandola.                 */
 void truncCreal(){
 	yylval.cent = atoi(yytext);
 	fprintf(stdout,"\nWarning at line %d: constante real truncada a entera %d\n",
 	yylineno,yylval.cent);
 }
 /********************************************************************************/
+
+
+
+
+
 
 
 
